86ba93a085834fa8a3ac49730365f76ef5ca43df
Brandon Aguirre <brag4918@colorado.edu>
2021-11-01 12:48:01 -0600

    fix: jest now works with @ alias

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
-  }
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    "moduleNameMapper": {
+        "@/(.*)": "<rootDir>/src/$1"
+    },
 };
